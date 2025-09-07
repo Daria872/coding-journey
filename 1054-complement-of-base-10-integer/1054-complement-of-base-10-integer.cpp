da88c8e61a13c8a1135d0de3eeee1eecd44e43a6
Daria872 <daria.josephine1@gmail.com>
2025-09-07 14:30:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (58.07%) - LeetHub

--- 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        
+        string rem="";
+        if(n==0) return 1;
+        while(n>0){
+            if(n%2==1){
+                rem+="1";
+            }
+            else{
+                rem+="0";
+            }
+            n/=2;
+        }
+        reverse(rem.begin(),rem.end());
+        string flip="";
+        for(int i=0;i<rem.size();i++){
+            if(rem[i]=='0')
+            flip+='1';
+            else
+            flip+='0';
+        }
+        int num=0,p2=1,x=flip.size();
+        for(int i=x-1;i>=0;i--){
+            if(flip[i]=='1'){
+                num+=p2;
+            }
+            p2*=2;
+        }
+        return num;
     }
 };
