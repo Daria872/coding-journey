415f35e2c646c30c1d0e629a539856b7e50a82c5
Daria872 <daria.josephine1@gmail.com>
2025-09-07 14:43:33 +0530

    Time: 0 ms (100%), Space: 32.7 MB (38.63%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
+    int atmostgoal(vector<int>&nums, int goal)
+    {
+        int l=0,r=0,s=0,cnt=0,n=nums.size();
+        if(goal<0) return 0;
+        while(r<n){
+            s+=nums[r];
+            while(s>goal){
+                s-=nums[l];
+                l++;
+            }
+            cnt+=(r-l+1);
+            r++;
+        }
+        return cnt;
+    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        
+        return atmostgoal(nums,goal)-atmostgoal(nums,goal-1);
     }
 };
