947d7038b1f46a987d00d5754cd5cb28bac7eef7
Daria872 <daria.josephine1@gmail.com>
2025-09-07 14:37:33 +0530

    Time: 0 ms (100%), Space: 7.8 MB (97.84%) - LeetHub

--- 1070-convert-to-base-2/1070-convert-to-base-2.cpp
+++ 1070-convert-to-base-2/1070-convert-to-base-2.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     string baseNeg2(int n) {
-        
+        string rem="";
+        if(n==0) return "0";
+        while(n!=0){
+            if((abs(n))%2==1){
+                rem+="1";
+                n=(n-1)/-2;
+            }
+            else{
+                rem+="0";
+                n=n/-2;
+            }
+        }
+        reverse(rem.begin(),rem.end());
+        return rem;
     }
 };
+
+
