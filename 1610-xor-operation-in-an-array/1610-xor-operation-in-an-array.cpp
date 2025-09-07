59f11186ceee281926ae8a570a675a7d6a7f3398
Daria872 <daria.josephine1@gmail.com>
2025-09-07 14:24:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (53.61%) - LeetHub

--- 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
+    int oper(int n){
+        if(n % 4 == 1) return 1;
+        else if(n % 4 == 2) return n + 1;
+        else if(n % 4 == 3) return 0;
+        else return n;
+    }
+
     int xorOperation(int n, int start) {
-        
+        int s = start >> 1;
+        int res = (s == 0 ? 0 : oper(s - 1)) ^ oper(s + n - 1);
+        return (res << 1) | (n & start & 1);
     }
 };
