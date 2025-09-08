9d5a8aef4874bb0e6b7216bc978827bd59ece468
Daria872 <daria.josephine1@gmail.com>
2025-09-09 04:09:36 +0530

    Time: 0 ms (100%), Space: 12.3 MB (8.99%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int>mpp;
+        vector<int>hash(2001,0);
         for(int i=0;i<arr.size();i++){
-            mpp[arr[i]]++;
+            hash[1000+arr[i]]++;
         }
-        unordered_set<int>st;
-        for(auto it:mpp){
-            if(st.find(it.second)!=st.end()){
+        sort(hash.begin(),hash.end());
+        for(int i=1;i<2001;i++){
+            if(hash[i]!=0 && hash[i-1]==hash[i]){
                 return false;
             }
-            st.insert(it.second);
         }
         return true;
     }
