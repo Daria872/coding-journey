1147cfa11c815d51d8322baa66a907dbaa6a949e
Daria872 <daria.josephine1@gmail.com>
2025-09-09 03:29:03 +0530

    Time: 118 ms (83.86%), Space: 169 MB (71.56%) - LeetHub

---
+++ 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        unordered_map<int,int>mpp;
+        for(int i=0;i<matches.size();i++){
+            int loser=matches[i][1];
+            mpp[loser]++;
+        }
+        vector<int>neverlost;
+        vector<int>lostonce;
+        for(int i=0;i<matches.size();i++){
+            int winner=matches[i][0];
+            int loser=matches[i][1];
+            if(mpp.find(winner)==mpp.end()){
+                neverlost.push_back(winner);
+                mpp[winner]=2;
+            }
+            if(mpp[loser]==1){
+                lostonce.push_back(loser);
+            }
+        }
+        sort(neverlost.begin(),neverlost.end());
+        sort(lostonce.begin(),lostonce.end());
+        return {neverlost,lostonce};
+    }
+};
