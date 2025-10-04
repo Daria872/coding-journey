348dab0c6b09d34430a3be3d8dc67d1fd7e8da3c
Daria872 <daria.josephine1@gmail.com>
2025-10-04 09:05:49 +0530

    Time: 0 ms (100.00%) | Memory: 24.4 MB (70.89%) - LeetSync

--- 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.cpp
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int evenNumberBitwiseORs(vector<int>& nums) {
-        vector<int>even;
-        int res=0;
+        int even_xor=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]%2==0)
-                even.push_back(nums[i]);
+                even_xor|=nums[i];
         }
-        for(int i=0;i<even.size();i++){
-            res=res|even[i];
-        }
-        return res;
+        return even_xor;
     }
 };
