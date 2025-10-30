b1eefe6391d038c72e71573c28be55a56b25a3a1
Daria872 <daria.josephine1@gmail.com>
2025-10-30 12:40:52 +0530

    Time: 39 ms (15.90%) | Memory: 45.2 MB (11.87%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void findCombination(int ind, vector<int>& candidates, int target, vector<int>ds, vector<vector<int>>&ans){
+        int n=candidates.size();
+        if(ind==n){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return ;
+        }
+        if(candidates[ind]<=target){
+            ds.push_back(candidates[ind]);
+            findCombination(ind,candidates,target-candidates[ind],ds,ans);
+            ds.pop_back();
+        }
+        findCombination(ind+1,candidates,target,ds,ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findCombination(0,candidates, target, ds, ans);
+        return ans;
+    }
+};
