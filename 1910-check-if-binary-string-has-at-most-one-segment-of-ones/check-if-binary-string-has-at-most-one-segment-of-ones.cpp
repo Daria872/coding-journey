acc2bd4253ba28d7c8caeaa02dd0acc9779b990a
Daria872 <daria.josephine1@gmail.com>
2026-03-06 14:06:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (77.26%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int i=0,n=s.size();
-        int count=0;
-        while(i<n){
-            if(s[i]=='1'){
-                count++;
-                while(i<n && s[i]=='1'){
-                    i++;
-                }
-            }
-            else{
-                i++;
-            }
-
-            if(count>1)
-                return false;
-        }
-        return true;
+        return s.find("01")==string::npos;
     }
 };
